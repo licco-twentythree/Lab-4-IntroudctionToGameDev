2fed3601944edf1535e23e73840a1751aa1ea0f9
licco-twentythree <licco23@gmail.com>
2022-08-23 18:01:14 +1000

    100 percent complete

--- Assets/PrintAndHide.cs
+++ Assets/PrintAndHide.cs
@@ -6,16 +6,32 @@ public class PrintAndHide : MonoBehaviour
 {
     private int i = 3;
     public Renderer rend;
+    private int random;
     // Start is called before the first frame update
     void Start()
     {
-        
+        random = Random.Range(200, 251);
     }
 
     // Update is called once per frame
     void Update()
     {
-        i++;
-        Debug.Log(gameObject.name + ":" + i);
+
+        if (gameObject.tag == "Red" && i > 99)
+        {
+            gameObject.SetActive(false);
+        }        
+
+        if (gameObject.tag == "Blue" && i == random)
+        {
+            rend.enabled = false;
+        }
+
+        
+        if (gameObject.active)
+        {
+            i++;
+            Debug.Log(gameObject.name + ":" + i);
+        }
     }
 }
